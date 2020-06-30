f73ada94a845105b5fbcb646531a5c31e5b96fc5
stevekutz <stkutz@gmail.com>
2020-06-29 21:24:39 -0500

    converted to using hooks, noting how state is replaced unlike merging with setState

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,27 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Person from './Person/Person'
 
-class App extends Component {
-    state = {
+const App = () => {
+    
+    const [personsState, setPersonsState ] = useState({
         persons: [
             {name: 'Joey', age: 22},
             {name: 'Alexander', age: 34},
             {name: 'Mike', age: 44},
-        ],
-        otherState: 'some value'
-    }
+            ],
 
-    switchNameHandler = () => {
-        // DO NOT try to mutate state directly  this.state.persons[0] = 'JoJo'
-        this.setState({
+        otherState: 'some value'
+    })
+
+    console.log(personsState);
+
+    const switchNameHandler = () => {
+        // Unlike how ssetState merges state, hooks will replace the previous state
+        // after switchNameHandler called, personState will not contain otherState
+        setPersonsState({
             persons: [
                 {name: 'JoeyChanged', age: 223},
                 {name: 'AlexanderUpdated', age: 342},
@@ -25,26 +30,26 @@ class App extends Component {
         })
     }
 
-    render() {
-        return (
-        <div className="App">
-            <h1> Starter App</h1>
-            {/* DO NOT use this {this.switchNameHandler()} as it will execute immediately when App is rendered and exceed Maximum Update depth */}
-            <button onClick = {this.switchNameHandler}> Switch Name</button>
 
-            {this.state.persons.map((item, index) => {
-                return (
-                    <Person key = {index} name = {item.name} age = {item.age}/>
-                )    
-            })
-            }    
-        </div>
-        )
+    return (
+    <div className="App">
+        <h1> Starter App</h1>
+        {/* DO NOT use this {this.switchNameHandler()} as it will execute immediately when App is rendered and exceed Maximum Update depth */}
+        <button onClick = {switchNameHandler}> Switch Name</button>
 
-
+        {personsState.persons.map((item, index) => {
+            return (
+                <Person key = {index} name = {item.name} age = {item.age}/>
+            )    
+        })
+        }    
+    </div>
+    )
 
 
-    }
+
+
+
 }
 
 export default App;
