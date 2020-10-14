89c9eec5ed840e4fe52da7aaa9dcd7250503e5dc
stevekutz <stkutz@gmail.com>
2020-10-13 22:06:00 -0500

    added styled-components

--- app/src/Person/Person.js
+++ app/src/Person/Person.js
@@ -1,9 +1,29 @@
 import React from 'react';
-// import Radium from 'radium'
+// import Radium from 'radium';
+import styled from 'styled-components';
 
 import './person.css'
 
 
+const StyledDiv = styled.div`
+    .personDiv {
+        width: 60%;
+        margin: 16px auto;
+        border: 1px solid #eee;
+        box-shadow: 0 2px 3px #eee;
+        padding: 16px;
+        text-align: center;
+    }
+
+
+    @media (max-width: 500px) {
+            width: 450px;
+            background-color: cadetblue;
+            margin: 0 auto;
+    }
+
+
+`
 
 const Person = (props) => {
 
@@ -18,12 +38,12 @@ const Person = (props) => {
 
 
     return (
-        <div  className = "personDiv" >
+        <StyledDiv  className = "personDiv" >
             <p onClick = {props.clickDelete}> 
                 My name is {props.name} and I am {props.age} years old, favorite number is {randomNum}</p>  
          {/*   <p> {props.children} </p>     adding 2-way binding by including props.name to pre-fill the form with orig name */}
             <input type = "text" onChange = {props.clickChanged} value = {props.name}/>
-        </div>
+        </StyledDiv>
     )
 }
 
