f6c412c1b8eea68aa2355bde596351456a90d81a
stevekutz <stkutz@gmail.com>
2020-10-14 19:39:37 -0500

    added styld components and dynamically rendered hover effects

--- app/src/App.js
+++ app/src/App.js
@@ -2,9 +2,39 @@ import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 // import Radium, {StyleRoot} from 'radium';
+import styled from 'styled-components';
 
 import Person from './Person/Person'
 var shortid = require('shortid');
+
+// use regular CSS properties inside  e.g. background-color instead of backgroundColor
+// to use dynamic styles inside with button background color
+// instead of  >>> background-color: seagreen;
+// add prop inside of actual Styled_Button     >>> list_displayed = {this.state.showPersons}
+// AND then use ternary to dynamically assign value as a string inside ` `
+
+// using a temmplate literal, we can insert an arrow function
+// ${props => props.list_displayed ? 'deeppink' : 'seagreen'};
+//
+// also, this can applied within hover
+
+const StyledButton = styled.button`
+    background-color: ${props => props.list_displayed ? 'deeppink' : 'seagreen'};
+    font: inherit;
+    border: 1px solid blue;
+    padding: 8px;
+    cursor: pointer;
+    margin: 2px;
+    border-radius: 5px;
+
+    &:hover {
+        background-color: ${props => props.list_displayed ? 'lightpink' : 'lightgreen'};
+        color: black;
+
+    }
+
+`;
+
 
 
 
@@ -49,26 +79,36 @@ class App extends Component {
     }
 
     inputNameHandler = (event, id) => {
-        // returns index if id is located in state
-        const personIndex = this.state.persons.findIndex( p => {
-            return p.id === id;
-        })
+        // // returns index if id is located in state
+        // const personIndex = this.state.persons.findIndex( p => {
+        //     return p.id === id;
+        // })
     
-        // create copy of state at located id
-        const person = {...this.state.persons[personIndex]};
+        // // create copy of state at located id
+        // const person = {...this.state.persons[personIndex]};
 
-        // create new data from event value info
-        person.name = event.target.value;
+        // // create new data from event value info
+        // person.name = event.target.value;
 
-        // make copy of state
-        const persons_copy = [...this.state.persons];
+        // // make copy of state
+        // const persons_copy = [...this.state.persons];
 
-        // set info at personIndex from copy of state
-        persons_copy[personIndex] = person;
+        // // set info at personIndex from copy of state
+        // persons_copy[personIndex] = person;
 
-        // set state with updated data
-        this.setState({persons: persons_copy});
+        // // set state with updated data
+        // this.setState({persons: persons_copy});
     
+
+        //  this should work
+        console.log(
+            '******** target name & value in updated handler',
+            event.target.name,
+            event.target.value
+        );
+
+        this.setState({[event.target.name]: event.target.value})
+
     
     }
 
@@ -93,21 +133,24 @@ class App extends Component {
 
     render() {
 
-     const style = {
-            backgroundColor: 'seagreen',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer',
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black',
+    //  const style = {
+    //         backgroundColor: 'seagreen',
+    //         font: 'inherit',
+    //         border: '1px solid blue',
+    //         padding: '8px',
+    //         cursor: 'pointer',
+    //         ':hover': {
+    //             backgroundColor: 'lightgreen',
+    //             color: 'black',
 
-            }
+    //         }
 
-     }
+    //  }
 
         let persons = null
+        
+        // upon re-render, a new random number is generated for all mapped items
+        //let randomNum = Math.floor(Math.random() * 30);
 
         if(this.state.showPersons) {
             persons = (
@@ -117,34 +160,22 @@ class App extends Component {
                         return <Person 
                             key = {person.id} 
                             name = {person.name} 
-                            age = {person.age} 
+                            age = {person.age}
+                            // fav_num = {randomNum} 
                             // readOnly = {!person.showPersons}
                             clickDelete = {() => this.deletePersonHandler(index)}
                             clickChanged = {(event) => this.inputNameHandler(event, person.id)}
                             />
                     })}
 
-                    {/*
-                    //  <Person 
-                    //       name = {this.state.persons[0].name} 
-                    //       age = {this.state.persons[0].age} />
-                    //  <Person 
-                    //       name = {this.state.persons[1].name} 
-                    //       age = {this.state.persons[1].age} 
-                    //       inputNewName = {this.inputNameHandler}
-                    //       clickPassedMethod = {this.switchNameHandler} > Hobbies are gaming, gaming, ... </Person>
-                    //  <Person 
-                    //       name = {this.state.persons[2].name} 
-                    //       age = {this.state.persons[2].age} />
-                    */}
                 </div>
 
             );
-                style.backgroundColor = 'deeppink';
-                style[':hover'] = {
-                    backgroundColor: 'lightpink',
-                    color: 'black', 
-                }
+                // style.backgroundColor = 'deeppink';
+                // style[':hover'] = {
+                //     backgroundColor: 'lightpink',
+                //     color: 'black', 
+                // }
         }
 
         // classnames are defined here to be set dynamically based on showPersons True of False
@@ -160,6 +191,7 @@ class App extends Component {
         }
 
 
+
         return (
 
             <div className="App">
@@ -171,17 +203,18 @@ class App extends Component {
                 <button onClick = {this.switchNameHandler}> Switch Name</button>
                 <button onClick = {() => this.switchNameHandler() }> Switch Name with Arrow func </button>
                 <button onClick = {this.changeFirstName.bind(this, 'NameChange with Bind' )}> Change First Name with Bind</button>
-                <button onClick = {() => this.changeFirstName('NameChange with Arrow')}>Change First Name with Arrow </button>
-                <button style = {style} onClick = {this.restoreState}> Restore State  </button>    
+                <button onClick = {() => this.changeFirstName('NameChange with Arrow')}>Change First Name with Arrow </button>   
                 */}
-                <button
-                        key = {() => shortid.generate()} 
-                        style = {style} 
-                        onClick = {this.restoreState}> Restore State  </button>
-                    <button
-                        style = {style}
-                        key = {2}
-                        onClick = {this.togglePersonsHandler}> Toggle Person components</button>       
+                <StyledButton
+                        //key = {() => shortid.generate()} 
+                        //style = {style} 
+                        // list_displayed = {this.state.showPersons}
+                        onClick = {this.restoreState}> Restore State  </StyledButton>
+                    <StyledButton
+                        //style = {style}
+                        //key = {2}
+                        list_displayed = {this.state.showPersons}
+                        onClick = {this.togglePersonsHandler}> Toggle Person components</StyledButton>       
                         {/*  added persons obj to be dynamically rendered */}
                         {persons}
     
--- app/src/Person/Person.js
+++ app/src/Person/Person.js
@@ -5,6 +5,28 @@ import styled from 'styled-components';
 import './person.css'
 
 
+// styled-components takes styles defined in component(s) and adds them as a class //
+// in the head tagof the rendered html a style tag is added by stled components:
+
+// created by styled-components
+// <style data-styled="active" data-styled-version="5.2.0">
+//  .kfMmkt 
+//      .personDiv{
+//          width:60%;
+//          margin:16px auto;
+//          border:1px solid #eee;
+//          box-shadow:0 2px 3px #eee;
+//          padding:16px;text-align:center;
+//      }
+//      @media (max-width:500px){
+//          .kfMmkt{
+//              width:450px;
+//              background-color:cadetblue;
+//              margin:0 auto;}}
+// </style>
+
+
+
 const StyledDiv = styled.div`
     .personDiv {
         width: 60%;
@@ -30,8 +52,9 @@ const Person = (props) => {
     let randomNum = Math.floor(Math.random() * 30);
     // const style = {
     //     '@media (min-width: 500px)':{
-    //         width: '400px',
-    //         background: 'cadetblue',
+    //          width: '400px',
+    //          background: 'cadetblue',
+    //          margin: 0 auto,
     //     }
     // }
 
