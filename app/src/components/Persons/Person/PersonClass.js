cebecc332accea27c2ed578235b57121773e4ad1
stevekutz <stkutz@gmail.com>
2020-10-21 21:35:05 -0500

    added methods to render adjacent components using HOC and Fragment

--- app/src/components/Persons/Person/PersonClass.js
+++ app/src/components/Persons/Person/PersonClass.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import classes from './person.module.css';
+import Aux from '../../../hoc/Aux';
+
 
 class PersonClass extends Component {
     
@@ -10,7 +12,9 @@ class PersonClass extends Component {
         console.log(' Person Class Component rendering')
         
         return (
-            <div  className = {classes.Person} >
+            <React.Fragment>           
+            {/* <Aux className = {classes.Person}>  */}
+            {/* <div  className = {classes.Person} > */}
                 <p onClick = {this.props.clickDelete}> 
                     My name is {this.props.name} and I am {this.props.age} years old</p>  
             {/*   <p> {props.children} </p>     adding 2-way binding by including props.name to pre-fill the form with orig name */}
@@ -18,7 +22,9 @@ class PersonClass extends Component {
                     type = "text" 
                     onChange = {this.props.clickChanged} 
                     value = {this.props.name}/>
-            </div>
+            {/* </div>  */}
+            {/*  </Aux>  */}
+            </React.Fragment>
         )
     
     
---
+++ app/src/hoc/Aux.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+// Called a HOC because it wraps another component(and could also add some extra logic or functionality)
+
+const aux = props => props.children;
+
+export default aux;
